498bddbdf13f9f964958e99037b619b82920c891
clairepender <clairepender5@gmail.com>
2021-10-26 22:29:49 -0500

    juicebox part 2 - part 1, beginning of Routes

--- index.js
+++ index.js
@@ -8,3 +8,20 @@ server.listen(PORT, () => {
 
 const apiRouter = require('./api');
 server.use('/api', apiRouter);
+
+
+const morgan = require('morgan');
+server.use(morgan('dev'));
+
+server.use(express.json())
+
+
+
+
+server.use((req, res, next) => {
+    console.log("<____Body Logger START____>");
+    console.log(req.body);
+    console.log("<_____Body Logger END_____>");
+  
+    next();
+  });
