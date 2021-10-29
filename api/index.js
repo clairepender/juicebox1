cb28d6611bd8c94922306929740f6146bb66a1c1
clairepender <clairepender5@gmail.com>
2021-10-28 21:45:33 -0500

    end of JSON Web Tokens, beginning of Protecting Our Secrets

--- api/index.js
+++ api/index.js
@@ -1,6 +1,37 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+server.use(async (req, res, next) => {
+    const prefix = 'Bearer '
+    const auth = req.headers['Authorization'];
+  
+    if (!auth) {
+      next(); // don't set req.user, no token was passed in
+    }
+  
+  
+    if (auth.startsWith(prefix)) {
+      // recover the token
+      const token = auth.slice(prefix.length);
+      try {
+        // recover the data
+        const { id } = jwt.verify(data, 'secret message');
+  
+        // get the user from the database
+        const user = await getUserById(id);
+        // note: this might be a user or it might be null depending on if it exists
+  
+        // attach the user and move on
+        req.user = user;
+  
+        next();
+      } catch (error) {
+        // there are a few types of errors here
+      }
+    }
+  })
+
+
 const usersRouter = require('./users');
 apiRouter.use('/users', usersRouter);
 
