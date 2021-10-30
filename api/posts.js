8a863ec8dc3230845a0cd112ea5425dd6c9020a5
clairepender <clairepender5@gmail.com>
2021-10-30 14:12:46 -0500

    beginning of writing patch api/posts/:id

--- api/posts.js
+++ api/posts.js
@@ -1,5 +1,8 @@
+const { response } = require('express');
 const express = require('express');
 const postsRouter = express.Router();
+const { getAllPosts, createPost } = require('../db');
+const { requireUser } = require('./utils');
 
 postsRouter.use((req, res, next) => {
     console.log("A request is being made to /posts");
@@ -7,7 +10,7 @@ postsRouter.use((req, res, next) => {
     next();
 });
 
-const { getAllPosts } = require('../db');
+
 
 postsRouter.get('/', async (req, res) => {
     const posts = await getAllPosts();
@@ -16,5 +19,33 @@ postsRouter.get('/', async (req, res) => {
         posts
     });
 });
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+    const { title, content, tags = "" } = req.body;
+  
+    const tagArr = tags.trim().split(/\s+/)
+    const postData = {};
+  
+    // only send the tags if there are some to send
+    if (tagArr.length) {
+      postData.tags = tagArr;
+    }
+  
+    try {
+      // add authorId, title, content to postData object
+      postData.authorId = req.user.id;
+      postData.title = title;
+      postData.content = content;
+
+      const post = await createPost(postData);
+      // this will create the post and the tags for us
+      // if the post comes back, res.send({ post });
+      res.send(post);
+      // otherwise, next an appropriate error object 
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  });
+  
 
 module.exports = postsRouter;
